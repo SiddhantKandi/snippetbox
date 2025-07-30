918b49cb503bdb281ac4eae4430b83b637cc67e2
Siddhant kandi <kandisiddhant@gmail.com>
2025-07-30 19:54:45 +0530

    Initialized the sever using http and servemux

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+)
+
+func home(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello from SnippetBox"))
+}
+
 
 func main(){
-	fmt.Println("Siddhant")
+	//HTTP Request Multiplier(Router).Dispatches the incoming requests to the matching URL path
+	mux := http.NewServeMux()
+	mux.HandleFunc("/",home)
+
+	log.Println("Server starting on 7001")
+	err := http.ListenAndServe(":7001", mux)
+	log.Fatal(err)
 }
