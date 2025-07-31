c7a44db1b65e2d5ad3bf202e516e40001bebb827
Siddhant kandi <kandisiddhant@gmail.com>
2025-07-31 15:57:00 +0530

    Added two routes 1. Create a snippet 2. View a snippet using servemux

--- main.go
+++ main.go
@@ -6,7 +6,29 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request){
+
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
+
+
 	w.Write([]byte("Hello from SnippetBox"))
+}
+
+func specificSpinnet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Showing a specific snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST" {
+		w.Header().Set("ALLOW", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+		return	
+	}
+
+	w.Write([]byte("Create a new snippet"))
 }
 
 
@@ -14,6 +36,9 @@ func main(){
 	//HTTP Request Multiplier(Router).Dispatches the incoming requests to the matching URL path
 	mux := http.NewServeMux()
 	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet/view",specificSpinnet)
+	mux.HandleFunc("/snippet/create",createSnippet)
+
 
 	log.Println("Server starting on 7001")
 	err := http.ListenAndServe(":7001", mux)
